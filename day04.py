6ecce8669d764398607b7e3cae5756acfd47cd74
Bianca Angelis <diangelis879@outlook.com>
2024-08-14 00:37:21 -0300

    chore: add extra day four challenge solution in file
    Signed-off-by: Bianca Angelis <diangelis879@outlook.com>

--- day04.py
+++ day04.py
@@ -14,6 +14,7 @@ def pop_out_duplicate_words(user_input):
     cleaned_word_list = list(dict.fromkeys(word_list))
     return ' '.join(cleaned_word_list)
 
+
 # Question 11
 # Write a program which accepts a sequence of comma separated 4 digit
 # binary numbers as its input and then check whether they are divisible by 5 or not.
@@ -25,3 +26,20 @@ def divisible_by_five(user_input):
         if int(value_list[i], 2) % 5 == 0:
             final_list.append(value_list[i])
     return ','.join(final_list)
+
+
+# Question 12
+# Write a program, which will find all such numbers between 2000 and 2010 (both included)
+# such that each digit of the number is an even number.The numbers obtained should be
+# printed in a comma-separated sequence on a single line.
+
+def get_only_all_even_digits_numbers():
+    final_number_list = []
+    for i in range(2000, 2010):
+        current_number = str(i)
+        number_digits = list(current_number)
+        if (int(number_digits[0]) % 2 == 0
+                and int(number_digits[1]) % 2 == 0
+                and int(number_digits[2]) % 2 == 0
+                and int(number_digits[3]) % 2 == 0): final_number_list.append(current_number)
+    return ','.join(final_number_list)
