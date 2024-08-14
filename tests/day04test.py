63d2cb752447bd947d71c94076c4cf1a8787f322
Bianca Angelis <diangelis879@outlook.com>
2024-08-13 23:50:09 -0300

    test: test file for day four challenges
    Signed-off-by: Bianca Angelis <diangelis879@outlook.com>

---
+++ tests/day04test.py
@@ -0,0 +1,21 @@
+import unittest
+from day04 import *
+
+class DayFourExercises(unittest.TestCase):
+    def test_string_sort(self):
+        expected_result = 'again and hello makes perfect practice world'
+        test_input = 'hello world and practice makes perfect again'
+        actual_result = sort_string_alphanumerically(test_input)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_popping_duplicates_out(self):
+        expected_result = 'again and hello makes perfect practice world'
+        test_input = 'again and and hello hello makes perfect practice world world'
+        actual_result = pop_out_duplicate_words(test_input)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_divisible_by_five(self):
+        expected_result = '1010'
+        test_input = '0100,0011,1010,1001'
+        actual_result = divisible_by_five(test_input)
+        self.assertEqual(expected_result, actual_result)
