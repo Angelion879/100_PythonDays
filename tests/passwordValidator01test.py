cb89dfa07193fdddf16b756056c186c557032c3e
Bianca Angelis <diangelis879@outlook.com>
2024-09-10 15:53:46 -0300

    update: password validator test file to check all needed password conditions
    Signed-off-by: Bianca Angelis <diangelis879@outlook.com>

--- tests/passwordValidator01test.py
+++ tests/passwordValidator01test.py
@@ -6,14 +6,42 @@ class ValidatorTest(unittest.TestCase):
 
     def test_valid_password_input(self):
         expected_result = 'Accepted'
-        test_input = 'password1'
+        test_input = 'P@ssword101%'
         actual_result = password_validator(test_input)
 
         self.assertEqual(expected_result, actual_result)
 
-    def test_invalid_password_input(self):
+    def test_short_password(self):
         expected_result = 'Unaccepted'
-        test_input = 'word'
+        test_input = 'W0rd$'
+        actual_result = password_validator(test_input)
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_no_specialcharacter_password(self):
+        expected_result = 'Unaccepted'
+        test_input = 'Password101'
+        actual_result = password_validator(test_input)
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_no_number_password(self):
+        expected_result = 'Unaccepted'
+        test_input = 'P@sswords'
+        actual_result = password_validator(test_input)
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_no_uppercase_password(self):
+        expected_result = 'Unaccepted'
+        test_input = 'p@ssword101'
+        actual_result = password_validator(test_input)
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_no_lowercase_password(self):
+        expected_result = 'Unaccepted'
+        test_input = 'P@SSWORD101'
         actual_result = password_validator(test_input)
 
         self.assertEqual(expected_result, actual_result)
