8e22b19e748ff029a172f3ff7e4f72b5beed0485
Bianca Angelis <diangelis879@outlook.com>
2024-09-10 15:55:00 -0300

    update: change regex condition to add more strict password requirements
    Signed-off-by: Bianca Angelis <diangelis879@outlook.com>

--- passwordValidator01.py
+++ passwordValidator01.py
@@ -2,6 +2,6 @@ import re
 
 
 def password_validator(user_password):
-    password_expected_pattern = re.compile(r"^.{8,}$")
+    password_expected_pattern = re.compile(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
 
-    return 'Accepted' if password_expected_pattern.search(user_password) else 'Unaccepted'
+    return 'Accepted' if password_expected_pattern.fullmatch(user_password) else 'Unaccepted'
